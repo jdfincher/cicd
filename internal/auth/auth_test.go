287890d0a9e8fd22938c7775fb7bc434aca93f38
jdfincher <jessefincher@protonmail.com>
2025-10-06 13:46:42 -0500

    ci tests should fail

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,31 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://localhost:8080", nil)
+	req.Header.Set("Authorization", "ApiKey somehashedtokenstring")
+
+	got, err := GetAPIKey(req.Header)
+	want := "somehashedtokenstring"
+	if err != nil {
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("expected: %v, got: %v", want, got)
+	}
+	req2, _ := http.NewRequest("GET", "", nil)
+	req2.Header.Set("Auth", "Authorization ApiKey")
+
+	got, err = GetAPIKey(req2.Header)
+	want = "should fail"
+	if !errors.Is(err, ErrNoAuthHeaderIncluded) {
+		t.Fatalf("expected: ' %v ', got: ' %v '", want, got)
+	}
+}
